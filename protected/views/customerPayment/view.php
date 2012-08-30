ef4c42ca1d314a66601baf5539f3744291d48a5b
Francis Beresford <francis.beresford@precedent.co.uk>
2012-08-30 21:07:42 +0800

    incorrectly named attribute

--- protected/views/customerPayment/view.php
+++ protected/views/customerPayment/view.php
@@ -13,7 +13,7 @@ $this->menu=array(
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id',
+		'payment_id',
 		'Customer.User.user_name',
 		'payment_type',
 		'payment_value',
