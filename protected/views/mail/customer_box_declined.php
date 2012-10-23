426c9374006ea05664c08c69697f4abb3d43319f
Donovan <donovan@snapfrozen.com.au>
2012-10-23 15:50:33 +1100

    added email templates

--- protected/views/mail/customer_box_declined.php
+++ protected/views/mail/customer_box_declined.php
@@ -2,12 +2,51 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Your order has been declined.</title>
+<title>Oh No! Your order has been declined.</title>
 </head>
 <body style="margin:0;">
-	<p>Hi <?php echo $Customer->User->first_name ?>,</p>
-	<h2>Your order has been declined.</h2>
-	<p>Your Bellofoodbox ID is: <strong><?php echo $Customer->User->bfb_id; ?></strong>. 
+<p>Hi <?php echo $Customer->User->first_name ?>,</p>
+
+<p>Unfortunately your Bellofoodbox order for 
+pick up on <?php $CustomerBox->Box->Week->week_delivery_date ?> has been DECLINED, as you 
+have insufficient credit.</p>
+
+<p>Please contact us immediately on if you feel this is an error.</p>
+
+<h3>Topping up your account</h3>
+
+<p>Please top up your Bellofoodbox account to 
+ensure you receive a delicious Bellofoodbox, 
+full of beautiful fresh, local produce <strong>next week</strong>.</p>
+
+<p>You can top up your balance directly using PayPal at:
+<a href="http://www.bellofoodbox.org.au/">www.Bellofoodbox.org.au</a></p>
+
+<p>We also accept Direct Deposits and Cash.</p>
+ 
+<p>When depositing money via bank transfer, please use your Bellofoodbox ID as your reference.<br />
+Your Bellofoodbox ID is: <strong><?php echo $Customer->User->bfb_id; ?></strong>.
+ 
+<p>(Payments by Cash are available anytime 
+at local Shop Front Order Boxes or in person 
+on Wednesday's Only during box pick up between 
+3.30pm - 6.30pm).</p>
+
+<p>
+--<br />
+Kind regards,<br />
+Box-o-matic Box Processing System</p>
+
+<p>Bellofoodbox is a community initiative promoting the local 
+economy, encouraging sustainable agricultural practices
+ and contributing to a fair, connected and resilient community. 
+Distributing local, seasonal, affordable, healthy, fresh food that 
+is accessible to Bellingen, Dorrigo, Urunga, and Coffs Harbour areas.</p>
+
+<p>Thank you for your generous support and being part of this wonderful project.</p>
+
+<p>Email: <a href="mailto:info@bellofoodbox.org.au">info@bellofoodbox.org.au</a><br />
+Phone: 0400 146 085</p>
 </body>
 </html>
 
--- protected/views/mail/customer_payment_receipt.php
+++ protected/views/mail/customer_payment_receipt.php
@@ -2,11 +2,56 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Payment receipt.</title>
+<title>Payment Confirmation</title>
 </head>
 <body style="margin:0;">
-	<p>Hi <?php echo $Customer->User->first_name ?>,</p>
-	<h2>Your payment has been added.</h2>
-	<p>Your Bellofoodbox ID is: <strong><?php echo $Customer->User->bfb_id; ?></strong>. 
+<p>Hi <?php echo $Customer->User->first_name ?>,</p>
+
+<p>Thank you for your Payment to Bellofoodbox.</p>
+
+<h3>Bellofoodbox payment details</h3>
+
+<p>Name: <?php echo $Customer->User->first_name ?> <?php echo $Customer->User->last_name ?><br />
+Date: <?php echo $Payment->payment_date ?><br />
+Amount: <?php echo $Payment->payment_value ?><br />
+Method: <?php echo $Payment->payment_type ?></p>
+
+<p><strong>Your current balance is $<?php echo $Customer->balance ?></strong></p>
+
+<p>This is NOT an order confirmation.
+Details confirming any box orders will be sent 
+in a separate email detailing pick up location and date.</p>
+
+<h3>Managing your Bellofoodbox account</h3>
+
+<p>You can check your account status, add credit and view previous 
+orders at: 
+
+<a href="http://www.bellofoodbox.org.au/">www.Bellofoodbox.org.au/</a></p>
+
+<p>You are welcome to top up your account 
+credit by making additional payments at anytime. 
+Box payments are only deducted each week as 
+your orders are processed.</p>
+
+<p>When depositing money via bank transfer, please use your Bellofoodbox ID as your reference.<br />
+Your Bellofoodbox ID is: <strong><?php echo $Customer->User->bfb_id; ?></strong>.
+
+<p>
+--<br />
+Kind regards,<br />
+Box-o-matic Box Processing System</p>
+
+<p>Bellofoodbox is a community initiative promoting the local 
+economy, encouraging sustainable agricultural practices
+ and contributing to a fair, connected and resilient community. 
+Distributing local, seasonal, affordable, healthy, fresh food that 
+is accessible to Bellingen, Dorrigo, Urunga, and Coffs Harbour areas.</p>
+
+<p>Thank you for your generous support and being part of this wonderful project.</p>
+
+<p>Email: <a href="mailto:info@bellofoodbox.org.au">info@bellofoodbox.org.au</a><br />
+Phone: 0400 146 085</p>
+
 </body>
 </html>
