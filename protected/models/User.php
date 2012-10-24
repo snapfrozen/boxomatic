1c9434991277373e58633d79deada03e6e37d970
Francis Beresford <francis.beresford@precedent.co.uk>
2012-10-24 13:01:35 +0800

    welcome email fix

--- protected/models/User.php
+++ protected/models/User.php
@@ -313,16 +313,15 @@ class User extends SnapActiveRecord
 		//$C->renderPartial('../mail/welcome',array('User'=>$this,'newPassword'=>$newPassword));
 		//echo '<br /><br />---------------------------------------<br /><br />';
 		
-		$email=trim($this->user_email);
-		//$message->addTo('donovan@snapfrozen.com.au');
-		//$message->addTo('leigh@bellofoodbox.org.au');
-		$message->addTo($email);
-		$message->from = Yii::app()->params['adminEmail'];
-
 		$validator=new CEmailValidator();
 		
 		if($validator->validateValue($email)) 
 		{
+			$email=trim($this->user_email);
+			//$message->addTo('donovan@snapfrozen.com.au');
+			//$message->addTo('leigh@bellofoodbox.org.au');
+			$message->from = Yii::app()->params['adminEmail'];
+			$message->addTo($email);
 			if(!@Yii::app()->mail->send($message))
 			{
 				return false;
