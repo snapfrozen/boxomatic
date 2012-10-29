4b995be168d3eb9c4353637324127a9fa805e7cc
Francis Beresford <francis.beresford@precedent.co.uk>
2012-10-29 10:14:26 +0800

    change reciever

--- protected/config/main.php
+++ protected/config/main.php
@@ -38,7 +38,7 @@ return array(
 				'username'=>'secretary_api1.northbankgarden.org.au',
 				'password'=>'8NGRF6NB9CZVGQ9M',
 				'signature'=>'AwKwAkKIBoRAzkQ2x1TE9DeYT9CxAA8nOeOXh3L0c9484nuuJ0Hd04uI',
-				'email'=>'secretary@paypal.com.au',
+				'email'=>'secretary@northbankgarden.org.au',
 				'identityToken'=>null,
 				//'identityToken'=>'YRkbQplC3JQjTdf_PaLnza2k2T5tZhQTGwtObQsenjQDxN07M5qLKKGNNu3u',
 			),
