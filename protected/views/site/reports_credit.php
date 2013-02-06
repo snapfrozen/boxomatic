2899323319826b45a65fb4fa5bdf834a484159a6
francisberesford <francis.beresford@gmail.com>
2013-02-06 13:18:00 +1100

    current credit by user

--- protected/views/site/reports_credit.php
+++ protected/views/site/reports_credit.php
@@ -52,4 +52,39 @@
 		));
 		endif; ?>
 	</div>
+	
+	<div class="clear"></div>
+	<h2>Current credit by user</h2>
+	
+	<table>
+		<thead>
+			<th>id</th>
+			<th>Name</th>
+			<th>Balance</th>
+		</thead>
+		<tbody>
+			<?php $Custs=Customer::model()->findAll();
+			$total=0;
+			foreach($Custs as $Cust):
+				$bal=(float)CHtml::value($Cust, 'balance');
+				$total+=$bal;
+			?>
+			<?php if($bal!=0): ?>
+			<tr>
+				<td><?php echo CHtml::value($Cust, 'User.id' ); ?></td>
+				<td><?php echo CHtml::value($Cust, 'User.first_name') . ' ' . CHtml::value($Cust, 'User.last_name') ?></td>
+				<td><?php echo Yii::app()->snapFormat->currency($bal) ?></td>
+			</tr>
+			<?php endif;?>
+			<?php endforeach;?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><?php echo Yii::app()->snapFormat->currency($total); ?></td>
+			</tr>
+		</tfoot>
+	</table>
+	
 </div>
