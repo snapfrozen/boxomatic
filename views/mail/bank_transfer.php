9ba288c0a2fc30a89b955f473405d205ea6a0dbb
Chienlv <levanchien.it@gmail.com>
2014-10-27 13:26:40 +0700

    Add Bank Transfer

---
+++ views/mail/bank_transfer.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+    hr.message-inner-separator
+    {
+        clear: both;
+        margin-top: 10px;
+        margin-bottom: 13px;
+        border: 0;
+        height: 1px;
+        background-image: -webkit-linear-gradient(left,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.15),rgba(0, 0, 0, 0));
+        background-image: -moz-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+        background-image: -ms-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+        background-image: -o-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+    }
+</style>
+<div class="row">
+    <div class="col-sm-12 col-md-12">
+        <div class="alert alert-info">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <span class="glyphicon glyphicon-info-sign"></span> <strong>Transfer information</strong>
+            <hr class="message-inner-separator">
+            <p class="note">Note : Please transfer the money into your account below</p>
+            <p><b>Account Name:</b> Mr Blogs</p>
+            <p><b>Account Number:</b> 321812812828</p>
+            <p><b>BSB:</b> 28282828282</p>
+        </div>
+    </div>
+</div>
+</div>
